5017de0a53a3bdd648547c13e0a9945c949d6fcc
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2024-03-16 07:29:29 +0700

    done caching

--- app/my-homes/page.tsx
+++ app/my-homes/page.tsx
@@ -3,8 +3,10 @@ import prisma from "../lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NoItems } from "../components/NoItems";
 import { ListingsCard } from "../components/Listings";
+import { unstable_noStore as noStore } from "next/cache";
 
 async function getData(userId: string) {
+  noStore();
   const data = await prisma.home.findMany({
     where: {
       userId,
